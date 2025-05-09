f642cfd291acefb112355dcfbc67b9227b97da02
dezsirazvan <dezsirazvan@gmail.com>
2025-05-10 00:40:35 +0200

    fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import EventsController from "./events_controller"
 
 const application = Application.start()
-application.register("events", EventsController)
 
 // Configure Stimulus development experience
 application.debug = false
